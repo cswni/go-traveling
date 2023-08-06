4b21eee9e39c6866a581e28f9076a9719d618e97
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2023-08-06 01:30:58 -0600

    Cambiada ruta relativa de las imágenes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { MdLocationOn } from 'react-icons/md';
 export const Header = () => {
   return (
     <header className='relative'>
-        <img src="/images/hotels/paris.jpg" alt="logo" />
+        <img src="images/hotels/paris.jpg" alt="logo" />
         <button className='flex gap-x-2 items-center left-1/2 -translate-x-1/2 absolute bottom-0 mb-4 bg-cyan-500 rounded-lg p-2 text-white'>
             <MdLocationOn className={"text-inherit"} />
             <span>Paris</span>
--- src/pages/Hotels.tsx
+++ src/pages/Hotels.tsx
@@ -4,24 +4,24 @@ export const Hotels = () => {
       name: 'Seminoles Hotel',
       starring: 4,
       price: 100,
-      image: '/images/hotels/seminole.jpg'
+      image: 'images/hotels/seminole.jpg'
     },
     {
       name: 'Princess Hilton',
       starring: 4.5,
       price: 125,
-      image: '/images/hotels/hilton.jpg'
+      image: 'images/hotels/hilton.jpg'
     },
     {
       name: 'Express Holiday Inn',
       starring: 4.5,
       price: 125,
-      image: '/images/hotels/express.jpg'
+      image: 'images/hotels/express.jpg'
     },{
       name: 'Crown Plaza',
       starring: 3.9,
       price: 95,
-      image: '/images/hotels/crown.jpg'
+      image: 'images/hotels/crown.jpg'
     },
   ]
   return (
