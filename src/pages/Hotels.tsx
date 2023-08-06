47fb93fb05ba0d3ab7b096cd1b2e5392e28308c8
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2023-08-06 12:06:01 -0600

    Meta tags

--- src/pages/Hotels.tsx
+++ src/pages/Hotels.tsx
@@ -31,7 +31,7 @@ export const Hotels = () => {
        <Title texto="Busca un hotel cerca de ti" />
         <div className="grid grid-cols-1 gap-4 mt-4 sm:grid-cols-2">
           {hotels.map((hotel) => (
-            <div className="overflow-hidden bg-white rounded-lg shadow-md hover:cursor-pointer">
+            <div key={hotel.name} className="overflow-hidden bg-white rounded-lg shadow-md hover:cursor-pointer">
               <img src={hotel.image} alt={hotel.name} className="object-cover object-center w-full h-56" />
               <div className="py-4">
                 <h3 className="flex justify-between mb-1 text-xs tracking-widest text-gray-500 title-font">{hotel.name}
